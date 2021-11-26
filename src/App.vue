d85e883bce94e6082030592c96859232c4c2d91f
zhangchengfei5 <537718696@qq.com>
2021-11-26 20:44:27 +0800

    pushu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <router-view />
+  <div class="all_wrapper">
+    <router-view />
+  </div>
 </template>
 
 <style>
+.all_wrapper {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+.all_wrapper::before {
+  content: "";
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url("../src/assets/images/bg.jpg") no-repeat;
+  background-size: cover;
+  -webkit-filter: blur(5px);
+  -moz-filter: blur(5px);
+  -o-filter: blur(5px);
+  -ms-filter: blur(5px);
+  filter: blur(5px);
+  z-index: -1;
+}
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,157 @@
 <template>
-  <div>嘀嘀嘀,你说什么呢??没反应啊</div>
+  <div class="home_wrapper">
+    <!-- S-顶部导航 -->
+    <div class="home_top_wrapper">
+      <!-- S-登录 -->
+      <div class="login_wrapper" @click="login">
+        <el-image :src="imgUrl.user"></el-image>
+        <p>登录</p>
+      </div>
+      <!-- E-登录 -->
+
+      <!-- S-搜索 -->
+      <div class="search_wrapper">
+        <el-icon color="#e6e6e6" :size="25"><arrow-left /></el-icon>
+        <el-input v-model="song" placeholder="搜索" clearable>
+          <template #prefix>
+            <el-icon class="el-input__icon"><search /></el-icon>
+          </template>
+        </el-input>
+        <el-icon color="#e6e6e6" :size="25"><microphone /></el-icon>
+      </div>
+      <!-- E-搜索 -->
+
+      <!-- S-主题、消息、关闭 -->
+      <div class="close_wrapper">
+        <el-icon color="#e6e6e6" :size="25"><setting /></el-icon>
+        <el-icon color="#e6e6e6" :size="25"><bell /></el-icon>
+        <el-icon color="#e6e6e6" :size="25"><full-screen /></el-icon>
+        <el-icon color="#e6e6e6" :size="25"><close /></el-icon>
+      </div>
+      <!-- E-主题、消息、关闭 -->
+    </div>
+
+    <!-- 主体 -->
+    <!-- E-顶部导航 -->
+  </div>
 </template>
 
 <script>
-export default {};
+import {
+  ArrowLeft,
+  Search,
+  Microphone,
+  Bell,
+  Close,
+  Setting,
+  FullScreen,
+} from "@element-plus/icons";
+
+export default {
+  components: {
+    ArrowLeft,
+    Search,
+    Microphone,
+    Bell,
+    Close,
+    Setting,
+    FullScreen,
+  },
+  data() {
+    return {
+      imgUrl: {
+        user: require("../assets/images/topNav/user.png"),
+        back: require("../assets/images/topNav/back.png"),
+      },
+      song: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("点击登录");
+    },
+  },
+};
 </script>
 
 <style>
+.home_wrapper {
+  width: 70vw;
+  height: 80vh;
+  box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.1);
+  margin: 0 auto;
+  margin-top: 50vh;
+  transform: translateY(-50%);
+}
+/* 顶部导航 */
+.home_top_wrapper {
+  width: 100%;
+  height: 10%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0.1rem;
+  background-color: #cc66ff;
+}
+/* 登录容器 */
+.login_wrapper {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.login_wrapper > p {
+  font-size: 0.18rem;
+  margin-left: 0.1rem;
+  color: #e6e6e6;
+}
+.login_wrapper > p:hover {
+  color: #fff;
+}
+/* 头像 */
+.login_wrapper .el-image {
+  width: 0.3rem;
+  height: 0.3rem;
+  border-radius: 50%;
+}
+.login_wrapper .el-image__inner {
+  width: 0.3rem;
+  height: 0.3rem;
+  border-radius: 50%;
+}
+/* 搜索容器 */
+.search_wrapper {
+  display: flex;
+  align-items: center;
+}
+.search_wrapper > .el-icon {
+  cursor: pointer;
+}
+.search_wrapper > .el-icon:hover {
+  color: #fff;
+}
+.search_wrapper > .el-input {
+  margin: 0 0.1rem;
+  width: 3rem;
+}
+.search_wrapper .el-input__inner {
+  height: 0.3rem;
+  min-height: 30px;
+  line-height: 0.3rem;
+}
+.search_wrapper .el-input__inner:focus {
+  border-color: #cc66ff;
+}
+/* 顶部右边 */
+.close_wrapper {
+  display: flex;
+  align-items: center;
+}
+.close_wrapper > .el-icon {
+  cursor: pointer;
+  margin-left: 0.2rem;
+}
+.close_wrapper > .el-icon:hover {
+  color: #fff;
+}
 </style>
